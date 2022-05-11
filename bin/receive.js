58034cd8c386831394cb1f23b97342997a108974
vlad_ <vladstitov@gmail.com>
2022-05-10 20:47:48 -0400

    merg

--- bin/receive.js
+++ bin/receive.js
@@ -13,6 +13,7 @@ const serialPort = new serialport_1.SerialPort({
 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
+        //  console.log('received ', data + '');
         serialPort.write(data + '\n');
     });
 });
