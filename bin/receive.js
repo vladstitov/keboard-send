2753c6ea12c6bd8c78a0cb456dd07fad14cc5392
vladstitov <vladstitov@gmail.com>
2022-05-14 17:57:56 -0400

    ssppeeeedd

--- bin/receive.js
+++ bin/receive.js
@@ -11,16 +11,31 @@ const serialPort = new serialport_1.SerialPort({
     path: 'COM5',
     baudRate: 115200
 });
+let serialdata = '';
+let lastSocket;
+serialPort.addListener('data', function (data) {
+    let str = data.toString();
+    serialdata += str;
+    if (str.includes('\n')) {
+        if (lastSocket)
+            lastSocket.send(serialdata);
+        serialdata = '';
+    }
+    console.log(data);
+});
 wss.on('connection', function connection(ws) {
     setTimeout(() => {
         console.log('connected state :', ws.readyState);
         ws.send('Welcome');
+        lastSocket = ws;
     }, 1000);
     ws.addEventListener('message', function (evt) {
         serialPort.write(evt.data.toString() + '\n');
     });
     ws.addEventListener('close', function (evt) {
         console.log('closed ', evt.code);
+        if (lastSocket === ws)
+            lastSocket = null;
     });
     ws.addEventListener('error', function (evt) {
         console.log('error ', evt);
--- src/receive.ts
+++ src/receive.ts
@@ -8,24 +8,37 @@ SerialPort.list().then(res => {
     // serialPort.write('ROBOT POWER ON')
 });
 
-const serialPort= new SerialPort( {
+const serialPort: SerialPort= new SerialPort( {
     path:'COM5',
     baudRate: 115200
 });
 
+let serialdata = '';
+let lastSocket: WebSocket;
+serialPort.addListener('data', function(data) {
+    let str: string = data.toString();
+    serialdata += str;
+    if(str.includes('\n')) {
+        if(lastSocket) lastSocket.send(serialdata);
+        serialdata = '';
+    }
+console.log(data);
+
+})
 
 wss.on('connection', function connection(ws:WebSocket) {
     setTimeout(() => {
         console.log('connected state :', ws.readyState);
         ws.send('Welcome')
+        lastSocket = ws;
     },1000);
-    ws.addEventListener('message', function (evt: MessageEvent) { 
-       
+    ws.addEventListener('message', function (evt: MessageEvent) {       
        
         serialPort.write(evt.data.toString() + '\n');
     });  
     ws.addEventListener('close', function (evt: CloseEvent) {
-          console.log('closed ', evt.code);         
+          console.log('closed ', evt.code); 
+          if(lastSocket === ws)  lastSocket = null;    
       }); 
 
       ws.addEventListener('error', function (evt: Event) {
